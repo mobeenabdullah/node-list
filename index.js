85010ee6bb169709a44c6228bbac04d50ee298ac
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-08 03:52:20 +0500

    Check if is file or folder

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if(err) {
         console.log(err);
     }
-    console.log(filenames);
+
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        console.log(filenames[index], stats.isFile());
+    }
 });
